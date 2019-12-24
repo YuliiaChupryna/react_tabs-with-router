f1dbbda436f81761bd42d29679178133d244c253
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-24 20:07:56 +0200

    Updates

---
+++ precache-manifest.bae395117cd70b0bc2546a0ba0f1dbbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29f563aa963402845676cf56e21e0aa",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "2afa0552a9f7596c1651",
+    "url": "/react_tabs-with-router/static/css/main.d157ffe8.chunk.css"
+  },
+  {
+    "revision": "a6ed9298720ddeba0193",
+    "url": "/react_tabs-with-router/static/js/2.605c7544.chunk.js"
+  },
+  {
+    "revision": "2afa0552a9f7596c1651",
+    "url": "/react_tabs-with-router/static/js/main.9ab10e36.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
